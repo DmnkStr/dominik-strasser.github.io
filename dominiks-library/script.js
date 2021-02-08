e9b726830fe215947be8ce6622f2d43a7fbdc5a8
Dominik <47605786+Dominik-Strasser@users.noreply.github.com>
2021-02-08 16:03:14 +0100

    Update

---
+++ dominiks-library/script.js
@@ -0,0 +1,12 @@
+//$.getJSON("data.json", function(json) {
+//    console.log(json); // this will show the info it in firebug console
+//});
+
+$(document).ready(function(){
+  $.getJSON("https://raw.githubusercontent.com/Dominik-Strasser/Dominik-Strasser.github.io/master/dominiks-library/data.json", function(data) {
+    console.log(data);
+
+
+  });
+
+});
