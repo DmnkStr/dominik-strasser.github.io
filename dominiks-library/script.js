fd731fb26fe311783d9ad82ade3879136787c9d6
Dominik <47605786+Dominik-Strasser@users.noreply.github.com>
2021-02-08 18:08:03 +0100

    Update

--- dominiks-library/script.js
+++ dominiks-library/script.js
@@ -4,7 +4,14 @@
 
 $(document).ready(function(){
   $.getJSON("https://raw.githubusercontent.com/Dominik-Strasser/Dominik-Strasser.github.io/master/dominiks-library/data.json", function(data) {
-    console.log(data.a);
+
+    for (i = 0; i < data.materials.length; i++) {
+      var name = data.materials[i].name;
+      var folder = name.replace(/ /g, '-').toLowerCase();
+      $("#container").append(name + ' - <figure class="image is-128x128"><img src="materials/' + folder + '/thumbnail.png"></figure><br>');
+    }
+
+
   })
   .fail(function(error) {
     console.log("error");
