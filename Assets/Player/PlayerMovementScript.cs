5e60fd705a912e011b5ef1fd342e10416b8b6173
Dominik <dominik_strasser@icloud.com>
2021-02-25 22:48:20 +0100

    Update

--- Assets/Player/PlayerMovementScript.cs
+++ Assets/Player/PlayerMovementScript.cs
@@ -10,6 +10,8 @@ public class PlayerMovementScript : MonoBehaviour
     private float playerSpeed = 5.0f;
     private float gravityValue = -9.81f;
 
+    private bool canRun = false;
+
     private void Start()
     {
         controller = GetComponent<CharacterController>();
@@ -17,18 +19,26 @@ public class PlayerMovementScript : MonoBehaviour
 
     void Update()
     {
+        Movement();
+    }
+
+    void Movement() 
+    {
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
         {
             //playerVelocity.y = 0f;
         }
 
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        if (move != Vector3.zero)
+        if (transform.GetChild(0).GetComponent<Animator>().GetCurrentAnimatorStateInfo(0).IsName("stand_up")) 
         {
-            gameObject.transform.forward = move;
+            Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+            controller.Move(move * Time.deltaTime * playerSpeed);
+
+            if (move != Vector3.zero)
+            {
+                gameObject.transform.forward = move;
+            }
         }
 
         playerVelocity.y += gravityValue * Time.deltaTime;
---
+++ Assets/Player/PlayerReleaseScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerReleaseScript : MonoBehaviour
+{
+    public GameObject stylizedCat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.anyKey)
+        {
+            transform.GetChild(0).GetComponent<Animator>().SetBool("isSitting", false);
+        }
+    }
+}
